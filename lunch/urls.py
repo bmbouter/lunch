a307f330f303ad361f74b9f274f1fa40fc5ee092
David White <dpwhite2@ncsu.edu>
2010-09-17 15:33:58 -0400

    Trying to resolve test errors.

--- lunch/urls.py
+++ lunch/urls.py
@@ -3,12 +3,12 @@ from django.conf.urls.defaults import *
 import datetime
 
 urlpatterns = patterns('lunch',
-    (r'^order/$', 'views.placeOrder'),
+    (r'^order/$', 'views.placeOrder', {},       'lunch-placeorder-view'),
     (r'^$', 'views.placeOrder'),
-    (r'^thanks/$', 'views.thanks'),
-    (r'^orders/$', 'views.viewOrders'),
-    (r'^orders/today/$', 'views.viewOrders', {'date': 'today'}),
-    (r'^orders/(?P<date>(\d{4}-\d{2}-\d{2}){1})/$', 'views.viewOrders'),
-    (r'^summary/$', 'views.viewOrdersSummary' ),
-    (r'^summary/(?P<year>\d{4})/(?P<month>\d{1,2})/$', 'views.viewOrdersSummary', {}, 'lunch-summary-view'),
+    (r'^thanks/$', 'views.thanks', {},          'lunch-thanks-view'),
+    (r'^orders/$', 'views.viewOrders', {},      'lunch-orders-view'),
+    (r'^orders/today/$', 'views.viewOrders', {'date': 'today'},                         'lunch-orderstoday-view'),
+    (r'^orders/(?P<date>(\d{4}-\d{2}-\d{2}){1})/$', 'views.viewOrders', {},             'lunch-ordersondate-view'),
+    (r'^summary/$', 'views.viewOrdersSummary', {},                                      'lunch-summarytoday-view'),
+    (r'^summary/(?P<year>\d{4})/(?P<month>\d{1,2})/$', 'views.viewOrdersSummary', {},   'lunch-summary-view'),
 )
