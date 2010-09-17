7aaf4d71a39c821a41105e97d7c7d310f31e2dc3
Brian Bouterse <bmbouter@gmail.com>
2010-09-17 13:41:02 -0400

    Added templates and set TEMPLATE_DIR in settings.py

--- settings.py
+++ settings.py
@@ -76,8 +76,11 @@ MIDDLEWARE_CLASSES = (
 )
 
 ROOT_URLCONF = 'lunch.urls'
+import os
+template_dir = os.getcwd() + '/templates'
 
 TEMPLATE_DIRS = (
+        template_dir,
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
